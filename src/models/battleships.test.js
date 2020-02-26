272dffeaf3d4ad50d4cfdbad292234d6720349b3
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-02-26 15:53:09 +0000

    install jest and enzyme dependencies

---
+++ src/models/battleships.test.js
@@ -0,0 +1,19 @@
+import Battleships from './battleships';
+
+describe('Battleships', () => {
+
+
+  beforeEach(() => {
+
+
+  });
+
+  // it('it should initialise a 10x10 array of empty cell objects', () => {
+  //   var expected;
+  //   var game
+  //   game = new Battleships();
+  //   expected = []
+  //   for(i = 0, i < 10; i++)
+  //   expect(game.render()).toEqual(30);
+  // });
+});
