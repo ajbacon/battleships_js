9ae638e67f257162fbd16eca6f065f491bfb32f3
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-02-27 18:30:57 +0000

    store a ship object

--- src/models/grid.js
+++ src/models/grid.js
@@ -1,29 +1,46 @@
 class Grid {
   constructor() {
-    this.currentGrid = this.newGrid()
+    this.currentGrid = this.newGrid();
   }
 
   render = () => {
-    return this.currentGrid
-  }
+    return this.currentGrid;
+  };
 
   placeShip = (location, length, direction) => {
-    for(let i = 0; i < length; i ++) {
-      this.currentGrid[location[0]][location[1] + i].value = 's'
+    let vStart = location[0];
+    let hStart = location[1];
+    for (let i = 0; i < length; i++) {
+      this.currentGrid[vStart][hStart].value = 's';
+      direction === 'H' ? hStart++ : vStart++;
     }
-  }
+  };
 
   newGrid = () => {
-    let emptyGrid = []
-    for(let i = 0; i < 10; i++) {
-      let newRow = []
-      for(let j = 0; j < 10; j++) {
-        newRow.push({ value: "-" })
+    let emptyGrid = [];
+    for (let i = 0; i < 10; i++) {
+      let newRow = [];
+      for (let j = 0; j < 10; j++) {
+        newRow.push({ value: '-' });
       }
-      emptyGrid.push(newRow)
+      emptyGrid.push(newRow);
     }
-    return emptyGrid
-  }
+    return emptyGrid;
+  };
+
+  ships = () => {
+    return [
+      {
+        length: 4,
+        coords: [
+          [2, 3],
+          [2, 4],
+          [2, 5],
+          [2, 6]
+        ]
+      }
+    ];
+  };
 }
 
 export default Grid;
--- src/models/grid.test.js
+++ src/models/grid.test.js
@@ -2,7 +2,7 @@ import Grid from './grid';
 import { emptyGrid } from '../test-helper';
 
 describe('Battleships', () => {
-  var grid
+  var grid;
   var expected;
 
   beforeEach(() => {
@@ -10,34 +10,66 @@ describe('Battleships', () => {
   });
 
   it('should initialise a 10x10 array of empty cell objects', () => {
-    expected = emptyGrid()
+    expected = emptyGrid();
     expect(grid.render()).toEqual(expected);
   });
 
-
   it('should place a ship of length 4 horizontally at position [0, 0]', () => {
-    expected = emptyGrid()
+    expected = emptyGrid();
 
-    expected[0][0].value = "s"
-    expected[0][1].value = "s"
-    expected[0][2].value = "s"
-    expected[0][3].value = "s"
+    expected[0][0].value = 's';
+    expected[0][1].value = 's';
+    expected[0][2].value = 's';
+    expected[0][3].value = 's';
 
-    grid.placeShip([0,0], 4, 'H')
+    grid.placeShip([0, 0], 4, 'H');
 
     expect(grid.render()).toEqual(expected);
   });
 
   it('should place a ship of length 4 horizontally at position [2, 3]', () => {
-    expected = emptyGrid()
+    expected = emptyGrid();
 
-    expected[2][3].value = "s"
-    expected[2][4].value = "s"
-    expected[2][5].value = "s"
-    expected[2][6].value = "s"
+    expected[2][3].value = 's';
+    expected[2][4].value = 's';
+    expected[2][5].value = 's';
+    expected[2][6].value = 's';
 
-    grid.placeShip([2,3], 4, 'H')
+    grid.placeShip([2, 3], 4, 'H');
 
     expect(grid.render()).toEqual(expected);
+  });
+
+  it('should place a ship of length 4 vertically at position [2, 3]', () => {
+    expected = emptyGrid();
+
+    expected[2][3].value = 's';
+    expected[3][3].value = 's';
+    expected[4][3].value = 's';
+    expected[5][3].value = 's';
+
+    grid.placeShip([2, 3], 4, 'V');
+
+    expect(grid.render()).toEqual(expected);
+  });
+
+  it('should store each ship as an object', () => {
+    expected = emptyGrid();
+
+    grid.placeShip([2, 3], 4, 'H');
+
+    expected = [
+      {
+        length: 4,
+        coords: [
+          [2, 3],
+          [2, 4],
+          [2, 5],
+          [2, 6]
+        ]
+      }
+    ];
+
+    expect(grid.ships()).toEqual(expected);
   });
 });
