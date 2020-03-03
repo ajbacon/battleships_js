98687d3607dfcf1d95bef88cbdc176adf9fb1d85
ajbacon <47368135+ajbacon@users.noreply.github.com>
2020-03-03 15:02:41 +0000

    change ship orientation

--- src/components/gridDisplay/gridDisplay.js
+++ src/components/gridDisplay/gridDisplay.js
@@ -4,16 +4,22 @@ import Grid from '../../models/grid/grid';
 
 class GridDisplay extends React.Component {
   state = {
-    model: new Grid()
+    model: new Grid(),
+    orientation: 'H'
   };
 
-  onClick = coord => {
+  handleCellClick = coord => {
     const updatedModel = this.state.model;
-    updatedModel.placeShip(coord, 4, 'H');
+    updatedModel.placeShip(coord, 4, this.state.orientation);
     this.setState({
       model: updatedModel
     });
-    console.log(coord);
+  };
+
+  toggleOrientation = () => {
+    this.setState({
+      orientation: this.state.orientation === 'H' ? 'V' : 'H'
+    });
   };
 
   renderRows = () => {
@@ -26,7 +32,7 @@ class GridDisplay extends React.Component {
           id={`row${i}`}
           cells={data[i]}
           yCoord={i}
-          onClick={this.onClick}
+          onClick={this.handleCellClick}
         />
       );
     }
@@ -34,7 +40,16 @@ class GridDisplay extends React.Component {
   };
 
   render() {
-    return <div data-test='component-grid-display'>{this.renderRows()}</div>;
+    return (
+      <div data-test='component-grid-display'>
+        {this.renderRows()}
+        <div>
+          <button onClick={this.toggleOrientation} data-test='orientation-btn'>
+            Orientation {this.state.orientation}
+          </button>
+        </div>
+      </div>
+    );
   }
 }
 
--- src/featureTests/placingShips.test.js
+++ src/featureTests/placingShips.test.js
@@ -10,7 +10,7 @@ describe('placingShips', () => {
   let wrapper;
 
   describe('placing a single ship', () => {
-    it('should be able to place a single, horizontal, ship of length 4 at locations [0,0]', () => {
+    it('should be able to place a single, horizontal, ship of length 4 at location [0,0]', () => {
       wrapper = mount(<GridDisplay />);
 
       inputCell = wrapper.find({ id: 'row0_cell0' });
@@ -20,6 +20,25 @@ describe('placingShips', () => {
       const cell2 = wrapper.find({ id: 'row0_cell1' });
       const cell3 = wrapper.find({ id: 'row0_cell2' });
       const cell4 = wrapper.find({ id: 'row0_cell3' });
+
+      expect(cell1.props().cellData.value).toEqual('s');
+      expect(cell2.props().cellData.value).toEqual('s');
+      expect(cell3.props().cellData.value).toEqual('s');
+      expect(cell4.props().cellData.value).toEqual('s');
+    });
+
+    it('should be able to place a single, vertical, ship of length 4 at location [0,0]', () => {
+      wrapper = mount(<GridDisplay />);
+
+      let orientationBtn = wrapper.find({ 'data-test': 'orientation-btn' });
+      orientationBtn.simulate('click');
+      inputCell = wrapper.find({ id: 'row0_cell0' });
+      inputCell.simulate('click');
+
+      const cell1 = wrapper.find({ id: 'row0_cell0' });
+      const cell2 = wrapper.find({ id: 'row1_cell0' });
+      const cell3 = wrapper.find({ id: 'row2_cell0' });
+      const cell4 = wrapper.find({ id: 'row3_cell0' });
 
       expect(cell1.props().cellData.value).toEqual('s');
       expect(cell2.props().cellData.value).toEqual('s');
